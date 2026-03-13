45beade5f2e4e0a02e85af6a824cc02018b0265a
reyvadin999 <reyvadin99@gmail.com>
2026-03-13 12:50:34 +0300

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -3,6 +3,6 @@ from flask_login import LoginManager
 
 db = SQLAlchemy()
 login_manager = LoginManager()
-login_manager.login_view = 'auth.login'
+login_manager.login_view = 'login'  # Change 'auth.login' to 'login'
 login_manager.login_message = "Please log in to access this page."
 login_manager.login_message_category = "warning"
